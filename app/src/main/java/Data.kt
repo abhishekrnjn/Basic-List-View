d45e7238e37c21d4232a2fde5927fd3979db799f
Abhishek Ranjan <abhishek.ranjan2690@gmail.com>
2020-07-25 14:38:30 +0530

    Created custom adapter

---
+++ app/src/main/java/Data.kt
@@ -0,0 +1,10 @@
+class Data {
+
+    var textNumber: String? = null
+    var audioNumber: String? =null
+
+    constructor(text: String, audio: String){
+        this.textNumber=text
+        this.audioNumber=audio
+    }
+}
--- app/src/main/java/com/example/simplelistview/MainActivity.kt
+++ app/src/main/java/com/example/simplelistview/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.example.simplelistview
 
+import Data
+import android.content.Context
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +21,94 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val arrayOfString = arrayOf<String>("Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten")
+        var arrayList: ArrayList<Data> = ArrayList()
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayOfString)
+        arrayList.add(Data("Zero","zeroaudio"))
+        arrayList.add(Data("One","oneaudio"))
+        arrayList.add(Data("Two","twoaudio"))
+        arrayList.add(Data("Three","threeaudio"))
+        arrayList.add(Data("Four","fouraudio"))
+        arrayList.add(Data("Five","fiveaudio"))
+        arrayList.add(Data("Six","sixaudio"))
+        arrayList.add(Data("Seven","sevenaudio"))
+        arrayList.add(Data("Eight","eightaudio"))
+        arrayList.add(Data("Nine","nineaudio"))
+        arrayList.add(Data("Ten","tenaudio"))
+
+
+        var adapter: NumberAdapter = NumberAdapter(this, arrayList)
         list_view.adapter = adapter
-g
+
 
     }
+
+
+}
+
+class NumberAdapter: BaseAdapter {
+
+    var arrayList: ArrayList<Data> = ArrayList()
+    var context: Context? = null
+
+    constructor(context: Context, arrayList: ArrayList<Data> ){
+        this.arrayList=arrayList
+        this.context=context
+    }
+
+
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var view: View
+        val inflater: LayoutInflater = context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val holder : ViewHolder
+        if(convertView == null){
+            view = inflater.inflate(R.layout.list_item,parent,false)
+            holder =ViewHolder()
+
+            holder.text = view.findViewById(R.id.text_view)
+            holder.image = view.findViewById(R.id.image_view)
+            view.tag = holder
+
+        }
+        else{
+            view = convertView
+            holder = convertView.tag as ViewHolder
+        }
+
+        val textValue = holder.text
+        textValue?.text = arrayList[position].textNumber
+        val imageValue = holder.image
+        var mediaplayer: MediaPlayer?
+
+        imageValue?.setOnClickListener{
+            mediaplayer = MediaPlayer.create(
+                context,context?.resources!!.getIdentifier(
+                    arrayList[position].audioNumber, "raw",context?.packageName))
+            mediaplayer?.start()
+        }
+        return view
+
+
+    }
+
+
+    private class ViewHolder{
+        var text: TextView? = null
+        var image: ImageView? = null
+    }
+
+    override fun getItem(position: Int): Any {
+        return arrayList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getCount(): Int {
+        return arrayList.size
+    }
+
 }
